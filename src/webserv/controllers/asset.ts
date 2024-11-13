fcd45eca9b3f1946b6722a5384487d5f277ae6e5
Amia <9750071+aamiaa@users.noreply.github.com>
2024-11-13 12:53:57 +0100

    fix: incorrect assets path

--- src/webserv/controllers/asset.ts
+++ src/webserv/controllers/asset.ts
@@ -7,7 +7,7 @@ export default class AssetController {
 	public static async serveNewEventAsset(req: Request, res: Response, next: NextFunction) {
 		const eventName = req.params.name
 		try {
-			const filePath = path.join(__dirname, "..", "..", "public", "assets", `event_${eventName}.png`)
+			const filePath = path.join(__dirname, "..", "..", "..", "public", "assets", `event_${eventName}.png`)
 			const writer = fs.createWriteStream(filePath)
 			const imgRes = await axios.get(`https://storage.sekai.best/sekai-en-assets/home/banner/event_${eventName}_rip/event_${eventName}.png`, {
 				responseType: "stream"
