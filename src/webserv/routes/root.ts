0d8319e7e9510e640ee6efd6941d6b18acab596d
Amia <9750071+aamiaa@users.noreply.github.com>
2024-11-20 13:29:50 +0100

    fix: static max-age

--- src/webserv/routes/root.ts
+++ src/webserv/routes/root.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 const mainRouter = Router()
 
-mainRouter.use("/", express.static(path.join(__dirname, "..", "..", "..", "public"), {maxAge: 14400}))
+mainRouter.use("/", express.static(path.join(__dirname, "..", "..", "..", "public"), {maxAge: 14400 * 1000}))
 mainRouter.get("/assets/event_:name.png", AssetController.serveNewEventAsset)
 
 export default mainRouter
