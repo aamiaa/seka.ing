5c1bb246e79f0a19b06bb440b2991bc0e97a5b3c
Amia <9750071+aamiaa@users.noreply.github.com>
2025-01-05 23:32:32 +0100

    remove debug leftover

--- src/webserv/controllers/time.ts
+++ src/webserv/controllers/time.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from "express";
 export default class TimeController {
 	public static async getServerTime(req: Request, res: Response, next: NextFunction) {
 		return res.json({
-			server_time: Date.now() + 6600
+			server_time: Date.now()
 		})
 	}
 }
