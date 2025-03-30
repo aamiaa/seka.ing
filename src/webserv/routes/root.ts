4413ab4964486861c3f14385b3852259d47e4f41
Amia <9750071+aamiaa@users.noreply.github.com>
2025-03-30 21:13:10 +0200

    decommission AssetController

--- src/webserv/routes/root.ts
+++ src/webserv/routes/root.ts
@@ -1,11 +1,9 @@
 import { Router } from "express"
-import AssetController from "../controllers/asset"
 import express from "express";
 import path from "path";
 
 const mainRouter = Router()
 
 mainRouter.use("/", express.static(path.join(__dirname, "..", "..", "..", "public"), {maxAge: 14400 * 1000}))
-mainRouter.get("/assets/event_:name.png", AssetController.serveNewEventAsset)
 
 export default mainRouter
