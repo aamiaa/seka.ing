17838ded381dd9417fa0a2e3971b6dde657317ae
Amia <9750071+aamiaa@users.noreply.github.com>
2025-07-22 20:59:26 +0200

    remove unused apis

--- src/webserv/controllers/event.ts
+++ src/webserv/controllers/event.ts
@@ -3,9 +3,7 @@ import SekaiMasterDB from "../../providers/sekai-master-db";
 import { PipelineStage } from "mongoose";
 import { SekaiEventType } from "../../interface/event";
 import { RankingSnapshotModel } from "../../models/snapshot/model";
-import { predict } from "../../util/math";
 import { EventProfileModel } from "../../models/event_profile";
-import { sha256 } from "../../util/hash";
 import CacheStore from "../cache";
 import { decryptEventSnowflake } from "../../util/cipher";
 import { EventProfileDTO } from "../dto/event_profile";
@@ -30,134 +28,6 @@ export default class EventController {
 	public static async getAnnouncements(req: Request, res: Response, next: NextFunction) {
 		res.set("Cache-Control", "no-store")
 		return res.json(CacheStore.get("cc_announce"))
-	}
-
-	public static async getPrediction(req: Request, res: Response, next: NextFunction) {
-		const rank = parseInt(req.query.rank as string)
-		const checkWorldlink = req.query.worldlink
-
-		const currentEvent = SekaiMasterDB.getCurrentEvent()
-		if(!currentEvent || Date.now() > currentEvent.aggregateAt.getTime()) {
-			return res.status(400).json({error: "No event in progress"})
-		}
-
-		let endDate = currentEvent.aggregateAt
-		const pipeline: PipelineStage[] = [
-			{
-				$match:
-				{
-					eventId: currentEvent.id
-				}
-			},
-			{
-				$unwind:
-				{
-					path: "$rankings"
-				}
-			},
-			{
-				$match:
-				{
-					"rankings.rank": rank
-				}
-			},
-			{
-				$project:
-				{
-					date: "$createdAt",
-					number: "$rankings.score",
-					_id: 0
-				}
-			}
-		]
-
-		if(checkWorldlink === "true" && currentEvent.eventType === SekaiEventType.WORLD_BLOOM) {
-			const currentChapter = SekaiMasterDB.getCurrentWorldBloomChapter()
-			endDate = currentChapter.aggregateAt
-			pipeline.splice(1, 0, {
-				$project: {
-					rankings: {
-						$arrayElemAt: ["$userWorldBloomChapterRankings.rankings", currentChapter.chapterNo - 1],
-					},
-					createdAt: 1
-				}
-			})
-		}
-
-		const dataPoints = await RankingSnapshotModel.aggregate(pipeline)
-		const prediction = predict(dataPoints, endDate)
-		return res.json({prediction: Math.floor(prediction)})
-	}
-
-	public static async getUserMatchesCount(req: Request, res: Response, next: NextFunction) {
-		const hash = req.query.hash
-		const checkWorldlink = req.query.worldlink
-
-		const currentEvent = SekaiMasterDB.getCurrentEvent()
-		if(!currentEvent || Date.now() > currentEvent.aggregateAt.getTime()) {
-			return res.status(400).json({error: "No event in progress"})
-		}
-
-		const userIds = (await EventProfileModel.find({}, {userId: 1})).map(x => x.userId)
-		const userId = userIds.find(x => 
-			hash === sha256(x + "_" + currentEvent.assetbundleName)
-		)
-		if(!userId) {
-			return res.status(400).json({error: "Unable to find user for the hash"})
-		}
-
-		const pipeline: PipelineStage[] = [
-			{
-				$match:
-				{
-					eventId: currentEvent.id
-				}
-			},
-			{
-				$unwind:
-				{
-					path: "$rankings"
-				}
-			},
-			{
-				$match:
-				{
-					"rankings.userId": userId
-				}
-			},
-			{
-				$project:
-				{
-					score: "$rankings.score"
-				}
-			},
-			{
-				$group:
-				{
-					_id: "$score"
-				}
-			},
-			{
-				$count: "matches"
-			}
-		]
-
-		if(checkWorldlink === "true" && currentEvent.eventType === SekaiEventType.WORLD_BLOOM) {
-			const currentChapter = SekaiMasterDB.getCurrentWorldBloomChapter()
-			pipeline.splice(1, 0, {
-				$project: {
-					rankings: {
-						$arrayElemAt: ["$userWorldBloomChapterRankings.rankings", currentChapter.chapterNo - 1],
-					}
-				}
-			})
-		}
-
-		const result = await RankingSnapshotModel.aggregate(pipeline)
-		if(result.length === 0) {
-			return res.json({matches: 0})
-		}
-		return res.json(result[0])
 	}
 
 	public static async getWorldlinkGraph(req: Request, res: Response, next: NextFunction) {
