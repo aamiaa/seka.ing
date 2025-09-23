4f15c091bcdf8ac39231e9db5f186544e99a46c6
Amia <9750071+aamiaa@users.noreply.github.com>
2025-09-23 18:35:42 +0200

    relax server side cache

--- src/sekai/event/maintenance.ts
+++ src/sekai/event/maintenance.ts
@@ -5,11 +5,6 @@ import CacheStore from "../../webserv/cache";
 
 export default class MaintenanceTracker {
 	public static async init() {
-		CacheStore.set("game_maintenance", {
-			active_maintenance: null,
-			planned_maintenances: []
-		})
-
 		await this.updateMaintenance()
 		setInterval(this.updateMaintenance.bind(this), 30 * 60 * 1000)
 		console.log("[MaintenanceTracker] Started!")
@@ -17,19 +12,7 @@ export default class MaintenanceTracker {
 
 	private static async updateMaintenance() {
 		const active = await this.getActiveMaintenance()
-		const planned = await this.getPlannedMaintenances()
-
-		CacheStore.set("game_maintenance", {
-			active_maintenance: active && active.endTime.getTime() > Date.now() ? {
-				starts_at: active.startTime,
-				ends_at: active.endTime
-			} : null,
-			planned_maintenances: planned.map(x => ({
-				starts_at: x.startsAt,
-				ends_at: x.endsAt,
-				news_id: x.newsId
-			}))
-		})
+		CacheStore.set("active_maintenance", active)
 
 		console.log("[MaintenanceTracker] Updated!")
 	}
@@ -48,16 +31,12 @@ export default class MaintenanceTracker {
 			return
 		}
 
-		const startTime = dateFromPTString(startDateStr)
-		const endTime = dateFromPTString(endDateStr)
+		const startsAt = dateFromPTString(startDateStr)
+		const endsAt = dateFromPTString(endDateStr)
 
 		return {
-			startTime,
-			endTime
+			startsAt,
+			endsAt
 		}
-	}
-
-	private static async getPlannedMaintenances() {
-		return await GameMaintenanceModel.find({startsAt: {$gte: new Date()}}).lean()
 	}
 }
