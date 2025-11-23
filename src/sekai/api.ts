732c790d541b92c8c9f95b2649ff459655cd7118
Amia <9750071+aamiaa@users.noreply.github.com>
2025-11-23 21:39:59 +0100

    move proxy idx to env file

--- src/sekai/api.ts
+++ src/sekai/api.ts
@@ -4,7 +4,7 @@ import SekaiApiClient from "sekai-api"
 const httpsAgent = HttpsProxyAgent({
 	host: process.env.ProxyHost,
 	port: parseInt(process.env.ProxyPort),
-	auth: `${process.env.ProxyUsername}-13:${process.env.ProxyPassword}`,
+	auth: `${process.env.ProxyUsername}:${process.env.ProxyPassword}`,
 })
 
 const client = new SekaiApiClient({
