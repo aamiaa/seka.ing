a193ab48697ca172be857afbace4f2b1df8ec132
Amia <9750071+aamiaa@users.noreply.github.com>
2026-02-28 02:03:51 +0100

    tweak: make paths not end with a trailing slash

--- src/webserv/routes/root.ts
+++ src/webserv/routes/root.ts
@@ -4,6 +4,25 @@ import path from "path";
 
 const mainRouter = Router()
 
-mainRouter.use("/", express.static(path.join(__dirname, "..", "..", "..", "public"), {maxAge: 14400 * 1000}))
+mainRouter.use("/", express.static(
+	path.join(__dirname, "..", "..", "..", "public"),
+	{
+		extensions: ["html"]
+	}
+))
+
+// Support old urls with trailing slash
+const legacyPaths = new Map([
+	["/events/", "events.html"],
+	["/titles/", "titles.html"],
+	["/wl-graph/", "wl-graph.html"]
+])
+mainRouter.use("/", (req, res, next) => {
+	if(legacyPaths.has(req.path)) {
+		const targetFile = legacyPaths.get(req.path)
+		return res.sendFile(targetFile, {root: "public"})
+	}
+	next()
+})
 
 export default mainRouter
